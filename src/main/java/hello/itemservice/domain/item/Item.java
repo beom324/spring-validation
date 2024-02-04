e244fdd34f2472fa2768ac455d25ce45ea3dcbc3
beom324 <scarlet2010@nate.com>
2024-02-04 15:11:47 +0900

    add ItemSaveForm , ItemEditForm

--- src/main/java/hello/itemservice/domain/item/Item.java
+++ src/main/java/hello/itemservice/domain/item/Item.java
@@ -12,18 +12,18 @@ import javax.validation.constraints.NotNull;
 //@ScriptAssert(lang = "javascript", script = "_this.price * _this.quantity>=10000", message = "수량*가격은 10000원 이상이어야합니다")
 public class Item {
 
-    @NotNull(groups = UpdateCheck.class)//수정요구사항
+//    @NotNull(groups = UpdateCheck.class)//수정요구사항
     private Long id;
     
-    @NotNull(groups = {SaveCheck.class, UpdateCheck.class})
+//    @NotNull(groups = {SaveCheck.class, UpdateCheck.class})
     private String itemName;
     
-    @NotNull(groups = {SaveCheck.class, UpdateCheck.class})
-    @Range(min = 1000, max = 1000000)
-    private Integer price;
+//    @NotNull(groups = {SaveCheck.class, UpdateCheck.class})
+//    @Range(min = 1000, max = 1000000)
+        private Integer price;
     
-    @NotNull(groups = {SaveCheck.class, UpdateCheck.class})
-    @Max(value = 9999,groups = {SaveCheck.class})
+//    @NotNull(groups = {SaveCheck.class, UpdateCheck.class})
+//    @Max(value = 9999,groups = {SaveCheck.class})
     private Integer quantity;
 
     public Item() {
